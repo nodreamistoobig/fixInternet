344919b3411713985c03d212ee66471af87278e9
nodreamistoobig <46135817+nodreamistoobig@users.noreply.github.com>
2020-09-12 14:23:38 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,55 @@
+package com.company;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Array;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int N, edges=0, a, b;
+        //try(FileReader reader = new FileReader("C:/Users/user/Desktop/Катя/Java/fixInternet/out/production/fixInternet/com/company/input.txt"))
+        try(FileReader reader = new FileReader("C:/Users/user/Desktop/Катя/Java/fixInternet/out/production/fixInternet/com/company/input2.txt"))
+        {
+            Scanner sc = new Scanner(reader);
+            N = sc.nextInt();
+            int[] isles = new int[N];
+            int i=0;
+            while(sc.hasNext()){
+                a = sc.nextInt() - 1;
+                b = sc.nextInt() - 1;
+                if (isles[a]==0){
+                    if (isles[b] == 0){
+                        edges++;
+                        isles[b]=isles[a]=edges;
+                    }
+                    else
+                        isles[a] = isles[b];
+                }
+                else {
+                    if (isles[b] == 0)
+                        isles[b] = isles[a];
+                    else {
+                        int oldIsle = isles[b];
+                        for (int j = 0; j < N; j++) {
+                            if (isles[i] == oldIsle){
+                                isles[i] = isles[a];
+                            }
+                        }
+                        edges--;
+                    }
+                }
+                i++;
+            }
+            for (int j = 0; j < N; j++)
+                if (isles[j]==0)
+                    edges++;
+            System.out.println(edges-1);
+        }
+        catch(IOException ex){
+
+            System.out.println(ex.getMessage());
+        }
+    }
+}
